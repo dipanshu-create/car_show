3ca5f835971cf17af0e03189d964fc49ad1f5cd1
da185214 <da185214@ncr.com>
2024-06-09 21:23:24 +0530

    adding api

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
 import Image from "next/image";
-import { Hero } from "@/components";
+import { CarCard, Hero } from "@/components";
 import SearchBar from "@/components/SearchBar";
 import CustomFilter from "@/components/CustomFilter";
+import { fetchCars } from "@/utils";
 
-export default function Home() {
+export default async function Home() {
+  const allCars = await fetchCars();
+  const isDataEmpty = !Array.isArray(allCars) || allCars.length < 1 || !allCars;
   return (
     <main className="overflow-hidden">
       <Hero />
@@ -19,6 +22,20 @@ export default function Home() {
             <CustomFilter title="fuel" />
           </div>
         </div>
+        {!isDataEmpty ? (
+          <section>
+            <div className="home__cars-wrapper">
+              {allCars?.map((car) => (
+                <CarCard car={car} />
+              ))}
+            </div>
+          </section>
+        ) : (
+          <div className="home__error-container">
+            <h2 className="text-black text-xl font-bold">Oops, no result</h2>
+            <p>{allCars?.message}</p>
+          </div>
+        )}
       </div>
     </main>
   );
--- utils/index.ts
+++ utils/index.ts
@@ -1,19 +1,37 @@
-const url = "https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=corolla";
-const options = {
-  method: "GET",
-  headers: {
+import { CarProps } from "@/types";
+
+export async function fetchCars() {
+  const headers = {
     "x-rapidapi-key": "708e4d60a1msh041ccb43c956246p1a85ffjsn0888b3fb726a",
     "x-rapidapi-host": "cars-by-api-ninjas.p.rapidapi.com",
-  },
-};
-
-try {
-  const response = await fetch(url, options);
-  const result = await response.text();
-  console.log(result);
-} catch (error) {
-  console.error(error);
+  };
+  const response = await fetch(
+    "https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=carrera",
+    {
+      headers: headers,
+    }
+  );
+  const result = await response.json();
+  return result;
 }
 
+export const calculateCarRent = (city_mpg: number, year: number) => {
+  const basePricePerDay = 50; // Base rental price per day in dollars
+  const mileageFactor = 0.1; // Additional rate per mile driven
+  const ageFactor = 0.05; // Additional rate per year of vehicle age
 
-export async function fetchCars=
+  // Calculate additional rate based on mileage and age
+  const mileageRate = city_mpg * mileageFactor;
+  const ageRate = (new Date().getFullYear() - year) * ageFactor;
+
+  // Calculate total rental rate per day
+  const rentalRatePerDay = basePricePerDay + mileageRate + ageRate;
+
+  return rentalRatePerDay.toFixed(0);
+};
+
+export const generateCarImageUrl = (car: CarProps, angle?: string) => {
+  const url = new URL("https://cdn.imagin.studio/getimage");
+  const { make, year, model } = car;
+  url.searchParams.append("customer", "hrjavascript-mastery");
+};
